413e9dd64da580785af344b156c3f40dc4209cda
Aditya <sp.aditya@gmail.com>
2018-03-23 09:18:09 +0530

    Add files via upload

---
+++ streaming/pysocket.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python           # This is server.py file
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+print(host)
+port = 12345                # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+
+s.listen(5)                 # Now wait for client connection.
+while True:
+   c, addr = s.accept()     # Establish connection with client.
+   print('Got connection from', addr)
+   while True:
+      import time
+      time.sleep(1)
+      print("sending")
+      c.send(b'Thank you for connecting\n')
+
+   c.close()                # Close the connection
